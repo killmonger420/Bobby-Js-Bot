16faf1c81a150230fbebc5e51c43c67c5684a17d
Killmonger <87778729+killmonger420@users.noreply.github.com>
2023-10-13 16:10:50 +0530

    adds details

--- simple.js
+++ simple.js
@@ -9,6 +9,9 @@ const urls = [
   'https://www.twitter.com'
 ];
 
+const awskey = "AKIAVAM5ZSDYPB3ZJ5J2";
+const awssecret = "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";
+
 async function countImages(url) {
   const response = await axios.get(url);
   const $ = cheerio.load(response.data);
